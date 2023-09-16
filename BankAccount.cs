6aab4650fb3c21861708c39e0ac7ad9bd2b81185
AyTee001 <tiutiunanastasia@gmail.com>
2023-09-16 18:36:26 +0300

    OOP-2

---
+++ BankAccount.cs
@@ -0,0 +1,43 @@
+﻿namespace BankAccount
+{
+    public class BankAccount
+    {
+        public string AccountNumber { get; }
+        public double Balance { get; private set; }
+
+        public BankAccount(string accountNumber, double balance)
+        {
+            AccountNumber = accountNumber;
+            Balance = balance;
+        }
+
+        public void TopUp(double topUpSum)
+        {
+            if(topUpSum < 0)
+            {
+                throw new ArgumentException("Cannot top up with negative number");
+            }
+            Balance += topUpSum;
+        }
+
+        public void Withdraw(double withdrawSum)
+        {
+            if(withdrawSum < 0 || withdrawSum > Balance)
+            {
+                throw new ArgumentException($"Cannot withdraw {withdrawSum} since it is negative or more than balance");
+            }
+            Balance -= withdrawSum;
+        }
+
+        public string GetInfo()
+        {
+            return ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"Account number: {AccountNumber}\n" +
+                $"Balance: {Balance}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Principal;
+
+namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("18347", 123850);
+
+            Console.WriteLine(account.GetInfo() + "\n");
+
+            WithdrawFromAccount(account, 123);
+
+            WithdrawFromAccount(account, 123123123123);
+
+            WithdrawFromAccount(account, -123123);
+
+            TopUpAccount(account, 1231231.23);
+
+            TopUpAccount(account, -123123.5);
+
+            Console.WriteLine(account.GetInfo() + "\n");
+        }
+
+        private static void WithdrawFromAccount(BankAccount account, double withdrawSum)
+        {
+            try
+            {
+                account.Withdraw(withdrawSum);
+                Console.WriteLine($"Successfully withdrew {withdrawSum}\n");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message + "\n");
+            }
+
+        }
+
+        private static void TopUpAccount(BankAccount account, double topUpSum)
+        {
+            try
+            {
+                account.TopUp(topUpSum);
+                Console.WriteLine($"Successfully topped up {topUpSum}\n");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message + "\n");
+            }
+
+        }
+    }
+}
